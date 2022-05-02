b8d0d8f6cda7fc25783aec72252ebf5f62239322
Battseren <battseren.102030@gmail.com>
2022-05-02 23:49:22 +0800

    ehnii push

--- src/screens/Bag/Index.js
+++ src/screens/Bag/Index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, StyleSheet, TextInput} from 'react-native';
+import {View, Text, StyleSheet, TextInput} from 'react-native'; 
 
 import ProductCardBag from '../../components/ProductCard/ProductCardBag';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
